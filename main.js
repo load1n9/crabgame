f20c329385928714eeef0c98ee6bbc075f094f02
loading <dsrebnik@gmail.com>
2020-04-19 13:04:41 -0400

    added mobile support

---
+++ main.js
@@ -0,0 +1,45 @@
+function preload() {
+   crabasset1 = loadImage("assets/crab.png")
+}
+//basic variables
+
+var p = {
+    x : 0,
+    y: 450,
+    w: 46.7,
+    h: 31.1,
+}
+
+
+// stuff
+function setup() {
+    createCanvas(500,500)
+}
+
+function draw() {
+    clear()
+    background("#aed2e8")
+    playerstuff()
+    
+}
+
+function playerstuff() {
+    image(crabasset1,p.x,p.y,p.w,p.h)
+    if (keyIsDown(LEFT_ARROW)) {
+        p.x -= 5;
+    }
+    
+    if (keyIsDown(RIGHT_ARROW)) {
+        p.x += 5;
+    }
+}
+
+
+
+function move1(){
+  p.x += 5;
+}
+
+function move2(){
+    p.x -= 5;
+}
